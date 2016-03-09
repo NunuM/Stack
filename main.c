cc3dc300d0b6b3a8d36f4b1c46add3eaf9234caa
Nuno Gonçalves <1140358@isep.ipp.pt>
2016-03-09 23:59:08 +0000

    Test

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 void printIntVec(int arr[], int n, struct Stack *st) {
     int i;
     for (i = 0; i < n; i++) {
-        printf("Elemento %d com valor de: %d\n", i, arr[i]);
+        printf("Element %d with value of: %d\n", i, arr[i]);
         push(st, arr[i]);
     }
 
@@ -15,21 +15,30 @@ void printIntStack(struct Stack *st) {
     int i;
     int size = st->length;
     for (i = 0; i < size; i++) {
-        printf("Elemto %d com valor de:%d\n", i, pop(st));
+        printf("Element %d with value of:%d\n", i, pop(st));
     }
+}
+
+int sizeMemoryAllocation(int n, int flag) {
+    if (flag)
+        return n << 1;
+
+
+    return n >> 1;
 }
 
 int main(int argc, char *argv[]) {
 
-    struct Stack *st = create_stack(0);
+    struct Stack *st = create_stack();
 
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    printf("Implementar uma Stak\n");
-    printf("\nValores a intruduzir\n");
-    printIntVec(arr, 8, st);
-    printf("\nResultado da stack\n");
+    printf("Implement a Stack\n");
+    printf("\nValues to insert\n");
+    printIntVec(arr, 10, st);
+    printf("\nStack Result\n");
     printIntStack(st);
+    destroyStack(st);
 
     return 0;
 }
